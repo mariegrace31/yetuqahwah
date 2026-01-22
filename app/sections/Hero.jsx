ce16b0beff913deba9bacb33b4ec52856a7ec97e
mariegrace <mariegracebmg@gmail.com>
2026-01-22 16:47:09 +0200

    Feature/add mob hero

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -6,35 +6,35 @@ function Hero() {
   return (
     <section
       id="acceuil"
-      className="relative w-full h-[108vh] flex items-center"
+      className="relative w-full h-[100vh] lg:h-[108vh] flex items-center"
     >
       <Image
         src={coffeecup}
         alt="Hero Image"
         fill
         priority
-        className="object-cover"
+        className="object-cover object-[59%_center] md:object-center"
       />
 
       <div className="absolute inset-0"></div>
 
-      <div className="relative z-10 max-w-2xl px-14">
-        <h1 className="uppercase text-[38px] font-montserrat leading-relaxed font-normal text-yq_choc">
-          marque de café <br /> congolaise de spécialité
+      <div className="relative z-10 max-w-2xl px-4 md:px-9 lg:px-14">
+        <h1 className="uppercase text-[20px] md:text-[25px] lg:text-[38px] w-[70%] lg:w-[100%] font-montserrat leading-relaxed font-normal text-yq_choc">
+          marque de café congolaise de spécialité
         </h1>
 
-        <p className="mt-6 text-yq_black font-light font-sans text-[15px] leading-relaxed">
+        <p className="mt-4 lg:mt-6 text-yq_black font-light font-sans w-[65%] lg:w-[100%] text-[11px] md:text-[12px] lg:text-[15px] leading-relaxed">
           Yetu Qahwah est un extraordinaire café, produit d'une collaboration
           entre les petits producteurs de café d'une part et les dégustateurs
           professionnels de café de spécialité d'autre part.
         </p>
 
-        <div className="mt-8 flex gap-6">
-          <button className="px-6 py-3 bg-yq_main text-white uppercase text-[13px] tracking-wide transition hover:opacity-90">
+        <div className="mt-5 lg:mt-8 flex gap-4 lg:gap-6">
+          <button className="px-4 lg:px-6 py-2 lg:py-3 bg-yq_main text-white uppercase text-[8px] md:text-[10px] lg:text-[13px] tracking-wide transition hover:opacity-90">
             notre boutique
           </button>
 
-          <button className="px-6 py-3 border border-yq_main text-yq_main uppercase text-[13px] tracking-wide transition hover:bg-yq_lightbeige">
+          <button className="px-4 lg:px-6 py-2 lg:py-3 border border-yq_main text-yq_main uppercase text-[8px] md:text-[10px] lg:text-[13px] tracking-wide transition hover:bg-yq_lightbeige">
             en savoir plus
           </button>
         </div>
