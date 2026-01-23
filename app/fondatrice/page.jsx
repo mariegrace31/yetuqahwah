38d853d96ffb0e7a46eb8b307b8de81c610239bb
mariegrace <mariegracebmg@gmail.com>
2026-01-23 19:29:08 +0200

    Improve the design

--- app/fondatrice/page.jsx
+++ app/fondatrice/page.jsx
@@ -9,12 +9,12 @@ function page() {
       <div>
         <Image src={image} width={100} height={100} alt='image' className="w-full h-[35vh] px-10 lg:hidden" />
         <h1 className="text-yq_choc uppercase text-center font-montserrat mt-6 lg:mt-0 text-[10px] md:text-[13px] lg:text-[16px]">Profil personnel</h1>
-      <div className='block lg:grid lg:grid-cols-2 gap-0 lg:gap-12 mt-2 lg:mt-10 px-5 lg:px-14 h-[51vh] lg:h-[85vh]'>
+      <div className='block lg:grid lg:grid-cols-2 gap-0 lg:gap-12 mt-2 lg:mt-10 px-5 lg:px-14'>
         <div>
           <Image src={image} width={100} height={100} alt='image' className="hidden lg:block lg:w-full lg:h-[75vh]" />
         </div>
 
-        <div className='flex flex-col gap-3 lg:gap-4 h-[75vh]'>
+        <div className='flex flex-col gap-3 lg:gap-4'>
           <h1 className='text-[12px] lg:text-[13px] text-yq_main mt-4 lg:mt-0 font-extralight uppercase'>Linda Mugaruka</h1>
           <p className='font-light text-[10px] md:text-[13px] lg:text-[16px] text-yq_black'>Linda Mugaruka est la fondatrice de Yetu Qahwah, une marque engagée dans la promotion du café congolais 
             à travers une approche inclusive et durable. Pionnière dans le secteur du café en République Démocratique 
@@ -34,25 +34,25 @@ function page() {
       </div>
       </div>
       
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 lg:gap-12 px-5 lg:px-14'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-7 lg:mt-16 lg:gap-12 px-5 lg:px-14'>
         <div className='shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[98%] mx-auto lg:w-full'>
           <Image src={image} width={100} height={100} alt='image' className='w-full h-[18vh] md:h-[20vh] lg:h-[25vh]' />
           <div className='px-4 py-6'>
-             <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue my-auto text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://dailycoffeenews.com/2021/09/14/three-questions-with-linda-mugaruka-the-queen-of-beans-in-the-drc/"> https://dailycoffeenews.com/2021/09/14/three-questions-with-linda-mugaruka-the-queen-of-beans-in-the-drc/</Link>
+             <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue my-auto text-[12px] break-all md:text-[14px] lg:text-[16px] font-light' href="https://dailycoffeenews.com/2021/09/14/three-questions-with-linda-mugaruka-the-queen-of-beans-in-the-drc/"> https://dailycoffeenews.com/2021/<br />09/14/three-questions-with-linda-mugaruka-the-queen-of-beans-in-the-drc/</Link>
           </div>
           </div>
 
          <div className='shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[98%] mx-auto lg:w-full'>
           <Image src={image} width={100} height={100} alt='image' className='w-full h-[18vh] md:h-[20vh] lg:h-[25vh]' />
           <div className='px-4 py-4 lg:py-6 flex'>
-            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue flex text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://www.youtube.com/watch?v=9gJUGZO_x00">https://www.youtube.com/watch?v=9gJUGZO_x00</Link>
+            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue flex text-[12px] break-all md:text-[14px] lg:text-[16px] font-light' href="https://www.youtube.com/watch?v=9gJUGZO_x00">https://www.youtube.com/watch?v=9gJUGZO_x00</Link>
           </div>
         </div>
 
          <div className='shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[98%] mx-auto lg:w-full'>
           <Image src={image} width={100} height={100} alt='image' className='w-full h-[18vh] md:h-[20vh] lg:h-[25vh]'  />
           <div className='px-4 py-4 lg:py-6'>
-             <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://time.com/collection/next-generation-leaders/4971126/linda-mugaruka-head-next-generation-leaders/">
+             <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue break-all text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://time.com/collection/next-generation-leaders/4971126/linda-mugaruka-head-next-generation-leaders/">
           https://time.com/collection/next-generation-leaders/4971126/linda-mugaruka-head-next-generation-leaders/</Link>
           </div>
         </div>
@@ -60,14 +60,14 @@ function page() {
          <div className='shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[98%] mx-auto lg:w-full'>
           <Image src={image} width={100} height={100} alt='image' className='w-full h-[18vh] md:h-[20vh] lg:h-[25vh]'  />
           <div className='px-4 py-4 lg:py-6'>
-            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://www.nytimes.com/2017/08/23/world/africa/democratic-republic-congo-specialty-coffee.html">https://www.nytimes.com/2017/08/23/world/ <br /> africa/democratic-republic-congo-specialty-coffee.html</Link>
+            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue break-all text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://www.nytimes.com/2017/08/23/world/africa/democratic-republic-congo-specialty-coffee.html">https://www.nytimes.com/2017/08/23/world/ <br /> africa/democratic-republic-congo-specialty-coffee.html</Link>
           </div>  
         </div>
 
         <div className='shadow-[0_4px_12px_rgba(0,0,0,0.1)] w-[98%] mx-auto lg:w-full'>
           <Image src={image} width={100} height={100} alt='image' className='w-full h-[18vh] md:h-[20vh] lg:h-[25vh]'  />
           <div className='px-4 py-4 lg:py-6'>
-            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://www.comunicaffe.com/fatuma-lokembo-renata-zanon-davide-spinelli-drink-congo-coffee-bwt-italia/">
+            <Link target="_blank" rel="noopener noreferrer" className='text-yq_blue break-all text-[12px] md:text-[14px] lg:text-[16px] font-light' href="https://www.comunicaffe.com/fatuma-lokembo-renata-zanon-davide-spinelli-drink-congo-coffee-bwt-italia/">
           https://www.comunicaffe.com/fatuma-lokembo-renata-zanon-davide-spinelli-drink-congo-coffee-bwt-italia/</Link>
           </div>  
         </div>
